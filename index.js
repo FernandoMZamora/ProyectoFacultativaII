4338c9b3b7a4d87a4465f5209c57c358d59d517e
jess97s <jessnangelsaballos@gmail.com>
2019-12-22 12:19:35 -0600

    Agregando dependencias

--- index.js
+++ index.js
@@ -5,8 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import Container from './componentes/container';
-import EventsView from './componentes/eventos';
 import ContenedorLista from './src/lista/ContenedorLista'
 
 AppRegistry.registerComponent(appName, () => ContenedorLista);
